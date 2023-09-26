2fac0924f0a58471ec76d29fe96948e7d475872a
1YouZhi1 <980507010@qq.com>
2023-09-26 20:23:31 +0800

    add ./entity/User.java

---
+++ user-service/src/main/java/com/test/entity/User.java
@@ -0,0 +1,26 @@
+package com.test.entity;
+
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+/**
+ * 用户实体类
+ *
+ * @Author YouZhi
+ * @Date 2023 - 09 - 26 - 19:56
+ */
+@Data
+@TableName("DB_USER")
+public class User {
+
+    @TableId(value = "uid", type = IdType.AUTO)
+    private Integer uid;
+
+    private String name;
+
+    private String sex;
+
+}
