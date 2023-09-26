67255b382efdf410168385411324fdb9893c52f6
1YouZhi1 <980507010@qq.com>
2023-09-26 21:23:42 +0800

    add ./mapper/BookMapper.java

---
+++ book-service/src/main/java/com/test/mapper/BookMapper.java
@@ -0,0 +1,13 @@
+package com.test.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.test.entity.Book;
+
+/**
+ * 书籍 mapper
+ *
+ * @Author YouZhi
+ * @Date 2023 - 09 - 26 - 21:22
+ */
+public interface BookMapper extends BaseMapper<Book> {
+}
