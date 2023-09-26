c913ff067c5b8bea667739d9219314f6e9fcd733
1YouZhi1 <980507010@qq.com>
2023-09-26 22:32:52 +0800

    add ./Service/BookService.java

---
+++ book-service/src/main/java/com/test/service/BookService.java
@@ -0,0 +1,19 @@
+package com.test.service;
+
+import com.test.entity.Book;
+
+/**
+ * 书籍 服务类
+ *
+ * @Author YouZhi
+ * @Date 2023 - 09 - 26 - 21:28
+ */
+public interface BookService {
+
+    /**
+     * 通过id查询书籍信息
+     * @param id
+     * @return
+     */
+    Book getBookById(int id);
+}
