42a116e4a3406c8a8729150a954e1e2acbfa8b77
1YouZhi1 <980507010@qq.com>
2023-09-26 20:24:21 +0800

    add ./mapper/UserMapper.java

---
+++ user-service/src/main/java/com/test/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.test.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.test.entity.User;
+
+/**
+ * @Author YouZhi
+ * @Date 2023 - 09 - 26 - 20:03
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
