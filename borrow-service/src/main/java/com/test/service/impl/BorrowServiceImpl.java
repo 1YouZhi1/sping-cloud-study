5d0ea1a3aa2afa6b939eb4aed1ab679b8c2410d8
1YouZhi1 <980507010@qq.com>
2023-09-27 16:13:17 +0800

    Implement load balancing

--- borrow-service/src/main/java/com/test/service/impl/BorrowServiceImpl.java
+++ borrow-service/src/main/java/com/test/service/impl/BorrowServiceImpl.java
@@ -10,6 +10,7 @@ import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,18 +26,20 @@ public class BorrowServiceImpl implements BorrowService {
     @Resource
     private BorrowMapper borrowMapper;
 
+    @Resource
+    private RestTemplate template;
+
     @Override
     public UserBorrowDetail getUserBorrowDetailByUid(int uid) {
         List<Borrow> borrow = borrowMapper.getBorrowsByUid(uid);
-        //RestTemplate支持多种方式的远程调用
-        RestTemplate template = new RestTemplate();
+
         //这里通过调用getForObject来请求其他服务，并将结果自动进行封装
         //获取User信息
-        User user = template.getForObject("http://localhost:8011/user/" + uid, User.class);
+        User user = template.getForObject("http://userservice/user/" + uid, User.class);
         //获取每一本书的详细信息
         List<Book> bookList = borrow
                 .stream()
-                .map(v -> template.getForObject("http://localhost:8021/book/" + v.getBid(), Book.class))
+                .map(v -> template.getForObject("http://bookservice/book/" + v.getBid(), Book.class))
                 .collect(Collectors.toList());
         return new UserBorrowDetail(user, bookList);
     }
---
+++ eureka-server/src/main/java/com/test/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author YouZhi
+ * @Date 2023 - 09 - 27 - 13:24
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
