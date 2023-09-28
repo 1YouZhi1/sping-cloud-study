56c4e1343c132d33945728eb498818e264af1e96
1YouZhi1 <980507010@qq.com>
2023-09-28 22:10:51 +0800

    为对应的服务指定负载均衡策略，随机策略   (不说洋文了，词穷

--- borrow-service/src/main/java/com/test/config/BeanConfiguration.java
+++ borrow-service/src/main/java/com/test/config/BeanConfiguration.java
@@ -1,12 +1,19 @@
 package com.test.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 
+/**
+ * @author YouZhi
+ * 指定为 userservice 服务，只要是调用此服务都会使用我们指定的策略
+ * 指定我们刚刚定义好的配置类
+ */
 @Configuration
+@LoadBalancerClient(value = "userservice", configuration = LoadBalancerConfig.class)
 public class BeanConfiguration {
     @Bean
     @LoadBalanced
